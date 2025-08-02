f0a64e05e467c04ac349beeaefde9ae45ad21756
Usman Tahir <Usman.tahir@synergy-it.com>
2025-08-02 16:30:33 +0500

    added config

---
+++ Config/CosmosDbConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgenticAI.Config
+{
+    public class BlobStorageConfig
+    {
+        public string AccountName { get; set; } = string.Empty;
+        public string AccountKey { get; set; } = string.Empty;
+        public string ContainerName { get; set; } = string.Empty;
+
+        public string ConnectionString { get; set; } = string.Empty;
+
+    }
+}
